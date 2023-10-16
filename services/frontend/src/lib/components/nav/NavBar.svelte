6f8e6db81fafc97635ab6b006598f210480a6db6
Marek Vospěl <marek@vospel.cz>
2023-10-16 20:24:33 +0200

    fix: room switching

--- services/frontend/src/lib/components/nav/NavBar.svelte
+++ services/frontend/src/lib/components/nav/NavBar.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte'
   import { roomsStore } from '../../../stores/matrix.store'
+
+  const dispatch = createEventDispatcher()
 
   export let extraClass = ''
 </script>
@@ -7,9 +10,12 @@
 <nav class="p-4 bg-gray-700 flex flex-col gap-2 {extraClass}">
   <nav class="flex flex-col">
     {#each $roomsStore.filter(r => r.getType() === undefined) as room (room.roomId)}
-      <a href="/app/room/{encodeURIComponent(room.roomId)}" class="w-full">
-        <button class="bg-transparent hover:bg-gray-800 px-2 py-0.25 rounded w-full text-left">{room.name}</button>
-      </a>
+      <button class="bg-transparent hover:bg-gray-800 px-2 py-0.25 rounded w-full text-left"
+        on:click={() => dispatch('routeSwitch', {
+          roomId: room.roomId
+        })}>
+        {room.name}
+      </button>
     {/each}
   </nav>
 </nav>
--- services/frontend/src/routes/app/+page.svelte
+++ services/frontend/src/routes/app/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-  import NavBar from '$lib/components/nav/NavBar.svelte';
+  import NavBar from '$lib/components/nav/NavBar.svelte'
+
+  let currentRoom: string | undefined = undefined
 </script>
 
 <div class="flex flex-row">
-  <NavBar extraClass="sticky top-0 h-screen" />
+  <NavBar extraClass="sticky top-0 h-screen" on:routeSwitch={(room) => currentRoom = room.detail.roomId}/>
   <main class="px-4 py-2">
+    <p>{ currentRoom }</p>
   </main>
 </div>
