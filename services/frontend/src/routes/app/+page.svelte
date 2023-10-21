33da91a46a2aae7d09b47e5883abe94d1be3f448
Marek Vospěl <marek@vospel.cz>
2023-10-21 15:21:13 +0200

    chore: input changes, scroll to bottom

--- services/frontend/src/routes/app/+page.svelte
+++ services/frontend/src/routes/app/+page.svelte
@@ -5,8 +5,17 @@
   import { currentRoomStore } from '../../stores/matrix.store'
   import { eventsStore } from '../../stores/matrix.store'
   import MessageEvent from '$lib/components/message/MessageEvent.svelte'
+  import { onMount } from 'svelte';
 
   let message = ''
+  let chatContainer: HTMLElement
+
+  function keydownSend(event: KeyboardEvent) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault()
+      send()
+    }
+  }
 
   async function send() {
     const msg = message
@@ -17,6 +26,8 @@
         msgtype: "m.text",
         body: msg,
       })
+      await new Promise(resolve => setTimeout(resolve, 0))
+      scrollToBottom()
     } catch(e) {
       console.warn(e)
     }
@@ -24,24 +35,41 @@
     message = ''
 
   }
+
+  const scrollToBottom = async () => {
+    await new Promise(resolve => setTimeout(resolve, 0))
+    if (!chatContainer)
+      return
+    chatContainer.scrollTop = chatContainer.scrollHeight
+  }
+  
+  onMount(() => {
+    scrollToBottom()
+  })
+
+  $: {
+    // Use the current room store so it gets updated when channel is changed
+    $currentRoomStore
+    scrollToBottom()
+  }
 </script>
 
 <div class="flex flex-row">
   <NavBar extraClass="sticky top-0 h-screen" on:routeSwitch={(room) => $currentRoomStore = room.detail.roomId}/>
-  <main class="py-2 w-full overflow-x-hidden break-words">
-    <p>{ $currentRoomStore }</p>
-
-    {#if $currentRoomStore}
-      <div class="flex flex-col gap-2">
-        {#each $eventsStore ?? [] as event (event.getId())}
-          <MessageEvent event={event} />
-        {/each}
-      </div>
-
-      <form class="w-full sticky bottom-2 pt-8 flex flex-row gap-2 px-4" on:submit|preventDefault={send}>
-        <input bind:value={message} placeholder="Message" class="text-black w-full" />
-        <button type="submit">Send</button>
-      </form>
-    {/if}
-  </main>
+  <div class="w-full h-screen overflow-y-auto relative2">
+    <main bind:this={chatContainer} class="py-2 w-full h-full overflow-x-hidden break-words">
+      <p>{ $currentRoomStore }</p>
+      {#if $currentRoomStore}
+        <div class="flex flex-col gap-2">
+          {#each $eventsStore ?? [] as event (event.getId())}
+            <MessageEvent event={event} />
+          {/each}
+        </div>
+        <form class="w-full sticky bottom-2 pt-8 flex flex-row gap-2 px-4" on:submit|preventDefault={send}>
+          <textarea bind:value={message} on:keydown={keydownSend} placeholder="Message" class="text-black w-full resize-none" />
+          <button type="submit">Send</button>
+        </form>
+      {/if}
+    </main>
+  </div>
 </div>
