aaecbea0c4616de71d7c79b016636d25ca4e4261
Marek Vospěl <marek@vospel.cz>
2023-12-27 17:37:38 +0100

    feat: finish emoji picker

--- services/frontend/src/lib/components/message/EmojiPicker.svelte
+++ services/frontend/src/lib/components/message/EmojiPicker.svelte
@@ -1,28 +1,49 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   import EmojiCategory from "./EmojiPicker/EmojiCategory.svelte";
   import groups from 'emojibase-data/meta/groups.json'
+    import { number } from "svelte-i18n";
 
   const dispatch = createEventDispatcher()
 
   let container: HTMLElement
+  let currentGroup = '0'
 
   function scrollToGroup(group: string) {
     container.querySelector(`[data-group="${group}"]`)?.scrollIntoView({ behavior: 'smooth' })
   }
+
+  onMount(() => {
+    container.addEventListener('scroll', () => {
+      const titles = Array.from(container.querySelectorAll('[data-group]'))
+      currentGroup = titles.find((title) => title.getBoundingClientRect().top > 0)?.getAttribute('data-group') ?? Object.keys(groups.groups)[Object.keys(groups.groups).length - 1]
+    })
+  })
 </script>
 
 <div class="flex flex-row gap-4 w-full max-h-75 overflow-y-auto relative rounded" bind:this={container}>
-  <div class="flex flex-col items-center gap-6 bg-surface2 h-full max-h-75 px-2 py-4 sticky top-0 overflow-y-auto">
+  <div class="flex flex-col items-center gap-2 bg-surface2 h-full max-h-75 px-2 py-4 sticky top-0 overflow-y-auto">
     {#each Object.keys(groups.groups) as group}
-      <button type="button" on:click={() => scrollToGroup(group)} class="flex items-center justify-center w-full h-full">
-        <span class="inline-block i-bxs:smile text-xl" />
+      <button type="button" on:click={() => scrollToGroup(group)} class="flex items-center justify-center w-full h-full p-1 rounded transition-colors duration-300 {currentGroup === group ? 'bg-surface1' : ''} hover:bg-surface1">
+        <span class="inline-block text-xl {
+          group == '0' ? 'i-bxs:smile' :
+          group == '1' ? 'i-bxs:like' :
+          group == '2' ? 'i-bxs:user' :
+          group == '3' ? 'i-bxs:cat' :
+          group == '4' ? 'i-bxs:pizza' :
+          group == '5' ? 'i-bxs:home' :
+          group == '6' ? 'i-bxs:bowling-ball' :
+          group == '7' ? 'i-bxs:wrench' :
+          group == '8' ? 'i-bxs:heart' :
+          group == '9' ? 'i-bxs:flag' :
+          ''
+        }" />
       </button>
     {/each}
   </div>
   <div class="flex-col gap-8 w-full max-w-75">
     {#each Object.keys(groups.groups) as group}
-      <EmojiCategory hiearchy={group} on:emoji={(emoji) => dispatch('emoji', emoji.detail)}/>
+      <EmojiCategory group={group} on:emoji={(emoji) => dispatch('emoji', emoji.detail)}/>
     {/each}
   </div>
 </div>
--- services/frontend/src/lib/components/message/EmojiPicker/EmojiCategory.svelte
+++ services/frontend/src/lib/components/message/EmojiPicker/EmojiCategory.svelte
@@ -2,24 +2,37 @@
   import { createEventDispatcher } from 'svelte';
   import emojis from 'emojibase-data/en/data.json'
   import groups from 'emojibase-data/meta/groups.json'
+  import text from 'emojibase-data/en/messages.json'
 
   const dispatch = createEventDispatcher()
 
-  export let hiearchy: string = '0'
+  export let group: any = '0'
 
   let open = true
 </script>
 
-<details bind:open={open} data-group={hiearchy}>
+<details bind:open={open} data-group={group} class="pt-4">
   <summary class="marker:content-none flex flex-row gap-1 items-center text-subtext0 hover:text-text duration-300 transition-colors">
-    <span class="i-bxs:smile inline-block" />
-    <span>Category</span>
+    <span class="inline-block text-xl {
+      group == '0' ? 'i-bxs:smile' :
+      group == '1' ? 'i-bxs:like' :
+      group == '2' ? 'i-bxs:user' :
+      group == '3' ? 'i-bxs:cat' :
+      group == '4' ? 'i-bxs:pizza' :
+      group == '5' ? 'i-bxs:home' :
+      group == '6' ? 'i-bxs:bowling-ball' :
+      group == '7' ? 'i-bxs:wrench' :
+      group == '8' ? 'i-bxs:heart' :
+      group == '9' ? 'i-bxs:flag' :
+      ''
+    }" />
+    <span>{ text.groups[group].message }</span>
     <span class="i-bx:chevron-down inline-block {open ? '' : 'rotate-180'}"/>
   </summary>
 
   <div class="flex flex-row items-center flex-wrap gap-2 pt-2">
-    {#each emojis.filter((e) => e.subgroup !== undefined && groups.hierarchy[hiearchy]?.includes(e.subgroup)).sort((a, b) => (a.order ?? 0) - (b.order ?? 0)) as emoji}
-      <button type="button" on:click={() => dispatch('emoji', emoji.emoji)}>
+    {#each emojis.filter((e) => e.subgroup !== undefined && groups.hierarchy[group]?.includes(e.subgroup)).sort((a, b) => (a.order ?? 0) - (b.order ?? 0)) as emoji}
+      <button type="button" on:click={() => dispatch('emoji', emoji.emoji)} class="p-1 rounded transition-colors duration-300 bg-transparent hover:bg-surface2">
         <img src="https://cdn.jsdelivr.net/gh/twitter/twemoji@v14.0.2/assets/svg/{emoji.hexcode.toLowerCase()}.svg" class="block w-8 h-8 overflow-hidden" alt={emoji.emoji}>
       </button>
     {/each}
